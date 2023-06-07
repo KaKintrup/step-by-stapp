b2c7ff79246c9e00f2f6eba814ce2f1f711e4939
Rahul Salotra <rs.salotra@gmail.com>
2023-06-07 14:21:03 +0200

    add joining and unjoining challenges

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :challenges do
-    resources :challenge_participations, only: [:create]
+    resources :challenge_participations, only: [:create, :index, :show, :edit, :update, :destroy]
   end
-  
-  resources :challenge_participations, only: [:index, :show, :edit, :update, :destroy]
-
 end
