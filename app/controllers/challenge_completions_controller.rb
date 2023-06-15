092fdd6f6709bc95d461d27f688c3ff389a03070
Rahul Salotra <rs.salotra@gmail.com>
2023-06-15 12:02:17 +0200

    calender finished

--- app/controllers/challenge_completions_controller.rb
+++ app/controllers/challenge_completions_controller.rb
@@ -3,6 +3,7 @@ class ChallengeCompletionsController < ApplicationController
   def create
     @completion = ChallengeCompletion.new
     @completion.challenge_participation_id = params[:challenge_participation_id]
+    @completion.start_time = params[:start_time]
     @completion.save
     redirect_back_or_to root_path
   end
